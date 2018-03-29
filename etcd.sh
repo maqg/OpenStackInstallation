652501276e75c31f36b66d337e921a6e2c368ff0
maqg <maquangang@163.com>
2018-03-29 02:58:54 -0400

    for keystone installation

---
+++ etcd.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#
+# For ETCD installation 
+# Henry.Ma
+#
+
+. ./configure.sh
+. ./functions.sh
+
+install_etcd
+echo -e "\e[1;32mInstall and Config ETCD OK! \e[0m"
---
+++ keystone.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+#
+# For OpenStack KeyStone Service in Controller node Installation
+# Henry.Ma
+#
+
+. ./configure.sh
+. ./functions.sh
+
+LANG=C
+LOGFILE=/var/log/openstack_install_keystone.log
+
+OS_VERSION=$(cat /etc/centos-release | cut -b 22)
+
+# Get platform and version info
+get_platform_info
+echo "Platform: \"$PLATFORM_TYPE\", Version: \"$PLATFORM_VERSION\"" >> $LOGFILE
+
+if [ "$OS_VERSION" != 7 ]; then
+	echo ""
+	echo -e "\e[1;31mNow only CentOs 7.X are supported, $PLATFORM_TYPE not supported now.\e[0m"
+	echo ""
+	exit 1
+fi
+
+install_keystone
+echo ""
+echo -e "\e[1;32mInstall KeyStone Service OK! \e[0m"
+echo ""
